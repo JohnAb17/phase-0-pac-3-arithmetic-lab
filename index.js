f8326871bf6041b35412cfe1d320683463e3dc3a
John Medina <john9713medina@hotmail.com>
2023-03-11 01:27:04 -0800

    lab complete

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a ,b) {
+    return a - b;
+}
+function multiply (a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    let value = ++n;
+    return (value);
+}
+function decrement(n) {
+    let value = --n;
+    return (value);
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+    
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
